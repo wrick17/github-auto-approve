857a3a9c188eb6617c76851dacd2b97b346370c1
Pratyush Poddar <pratyush.poddar@sequoia.com>
2024-08-29 10:22:13 +0530

    add check icon

--- content.js
+++ content.js
@@ -1,3 +1,7 @@
+const svg = `<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-check">
+    <path d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"></path>
+</svg>`;
+
 const approve = () => {
   const reviewButton = document.querySelector(".js-reviews-toggle");
   reviewButton.click();
@@ -27,7 +31,7 @@ const checkForReviewButton = (cb) => {
 const addAutoApproveButton = () => {
   const autoApproveButton = document.createElement("button");
   autoApproveButton.id = "auto-approve";
-  autoApproveButton.innerHTML = "Blindly Auto-Approve";
+  autoApproveButton.innerHTML = `${svg} Blindly Auto-Approve`;
   autoApproveButton.classList = "auto-approve";
   document.querySelector(".gh-header-meta").appendChild(autoApproveButton);
 
